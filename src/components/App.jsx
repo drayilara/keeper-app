a975df472b4c2ff1f00ef32362da747bb238e6d5
unknown <ayilarasodiq1@gmail.com git config --global user.name>
2022-06-07 22:28:09 +0100

    Added props to Keeper app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,28 @@ import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Note from "./Note";
+import notes from "../notes";
+
+
+
+
 
 
 export default function App() {
     return (
-        <React.Fragment>  
+        <React.Fragment> 
         <Navbar />
-        <Note />
+
+        {
+            notes.map(note => 
+            <Note 
+            key ={note.key}
+            title={note.title}
+            content={note.content}
+            />    
+        )
+
+        }   
         <Footer />
         </React.Fragment>   
     )
